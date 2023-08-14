c10c257619569e5c105fa754d663bfb5fbe57baf
cuilan <419475937@qq.com>
2023-08-14 15:50:24 +0800

    auto commit and push by cuilan on 2023-08-14 15:50:24

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-### Hi there 👋
+## Hi there 👋
 
 - 🤔 How to Fuck me?
 - 🌱 Give me Green hat！
 
-### This Year
-
-⏳ Year progess ${progressBarOfThisYear} ${(progressBarOfThisYear * 100).toFixed(2)} %
-
 ---
 
+### This Year
+
+⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
 ⏰ Updated on ${new Date().toUTCString()}
+
+---
 
 ### My Github Contributions
 
--- index.js
+++ index.js
@@ -14,18 +14,20 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
+## Hi there 👋
 
 - 🤔 How to Fuck me?
 - 🌱 Give me Green hat！
 
-### This Year
-
-⏳ Year progess ${progressBarOfThisYear} ${(progressBarOfThisYear * 100).toFixed(2)} %
-
 ---
 
+### This Year
+
+⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
 ⏰ Updated on ${new Date().toUTCString()}
+
+---
 
 ### My Github Contributions
 
